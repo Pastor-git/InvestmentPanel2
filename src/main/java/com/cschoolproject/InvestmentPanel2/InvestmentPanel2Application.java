8ccffdd24eb76efde989da3221c7ebd8533e3571
juskazet <j.zagorowicz@gmail.com>
2020-05-29 14:43:09 +0200

    first

---
+++ src/main/java/com/cschoolproject/InvestmentPanel2/InvestmentPanel2Application.java
@@ -0,0 +1,13 @@
+package com.cschoolproject.InvestmentPanel2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentPanel2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentPanel2Application.class, args);
+	}
+
+}
